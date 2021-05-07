c3fd6e45a7580ca4e294e256ef0f86a54e0bf1b0
Naomi Hauret <hauretnaomi@gmail.com>
2021-05-07 22:41:08 +0200

    style: prettier format

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,46 +7,40 @@ import { RichTextBlock } from 'prismic-reactjs'
 import { fetchLatestGithubRepo } from '@lib/api'
 
 interface IGithubRepository {
-  name: string,
-  html_url: string,
-  description?: string
+  name: string;
+  html_url: string;
+  description?: string;
 }
 interface IInternalLink {
-  title: string,
-  href: string
+  title: string;
+  href: string;
 }
 
 interface IPageHomeProps {
-  repositories: IGithubRepository[],
+  repositories: IGithubRepository[];
   content: {
     h1_title: string,
     section_repositories_title: string,
     main_text: RichTextBlock[],
     rich_text: RichTextBlock[],
-  },
-  articles: IInternalLink[]
+  };
+  articles: IInternalLink[];
 }
 
 const Page = ({ content, articles, repositories }: IPageHomeProps) => {
   return (
     <main>
-      <h1>
-        {content.h1_title}
-      </h1>
+      <h1>{content.h1_title}</h1>
       <section>
-        <h2>
-          {content.section_repositories_title}
-        </h2>
+        <h2>{content.section_repositories_title}</h2>
         <ul>
-          {repositories.map((repo, i) => <li data-testid='repository' key={i}>
-            {repo.name}
-            <p>
-              {repo.description}
-            </p>
-            <a href={repo.html_url}>
+          {repositories.map((repo, i) => (
+            <li data-testid="repository" key={i}>
               {repo.name}
-            </a>
-          </li>)}
+              <p>{repo.description}</p>
+              <a href={repo.html_url}>{repo.name}</a>
+            </li>
+          ))}
         </ul>
       </section>
     </main>
@@ -54,7 +48,7 @@ const Page = ({ content, articles, repositories }: IPageHomeProps) => {
 }
 
 interface IPageHome_GetStaticProps {
-  locale: string
+  locale: string;
 }
 
 export async function getStaticProps({ locale }: IPageHome_GetStaticProps) {
@@ -78,11 +72,11 @@ export async function getStaticProps({ locale }: IPageHome_GetStaticProps) {
       props: {
         content: content.results[0].data,
         articles: listArticles,
-        repositories: repositories.map((repository: IGithubRepository) => ({ // Should be another type here that would fully describe a Github repository but oh well
+        repositories: repositories.map((repository: IGithubRepository) => ({
+          // Should be another type here that would fully describe a Github repository but oh well
           name: repository.name,
           html_url: repository.html_url,
-          description: repository.description
-        
+          description: repository.description,
         })),
       },
     }
