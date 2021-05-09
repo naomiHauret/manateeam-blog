2f4a780892900872d69f5cc3a864fc540977df20
Naomi Hauret <hauretnaomi@gmail.com>
2021-05-09 21:59:53 +0200

    test(homepage): add axe to e2e tests

--- cypress/integration/homepage/index.spec.js
+++ cypress/integration/homepage/index.spec.js
@@ -1,11 +1,20 @@
 describe('As a visitor on the homepage', () => {
     beforeEach(() => {
         cy.visit('/')
+        cy.injectAxe()
     })
-      
+    
     it('I can load the page', () => {
         cy.visit('/')
     })
+
+    it('No critical a11y violations prevent me to use the page', () => {
+        // Test on initial load, only report and assert for critical impact items
+        cy.checkA11y(null, {
+        includedImpacts: ['critical']
+        })
+    })
+
     it('I can see the latest Manateem Github repositories', () => {
         cy.get('[data-testid=repository]').should('have.length', 1)
     })
